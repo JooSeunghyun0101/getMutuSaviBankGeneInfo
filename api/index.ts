e18232ac8c4048591a5c4e55c67390f5fde66c0d
JooSeunghyun0101 <carefree0101@gmail.com>
2025-01-01 15:48:35 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/weather", function (req: any, res: any) {
+app.get("/getMutuSaviBankGeneInfo", function (req: any, res: any) {
   const { serviceKey, numOfRows, pageNo, title, basYm } =
     req.query;
 
@@ -26,6 +26,6 @@ app.get("/weather", function (req: any, res: any) {
 
 app.listen(3000, function () {
   console.log(
-    "http://127.0.0.1:3000/weather?serviceKey=QxyJV9LnhlKd8GFOYmhwsVaNY7yRNSKdgoZs3SctHvq9LEQFyi298mJesKwyZj7cHuy4N0jHztZEy9x7djZxeQ%3D%3D&numOfRows=100&pageNo=1&basYm=202409 app listening on port 3000!"
+    "http://127.0.0.1:3000/getMutuSaviBankGeneInfo?serviceKey=QxyJV9LnhlKd8GFOYmhwsVaNY7yRNSKdgoZs3SctHvq9LEQFyi298mJesKwyZj7cHuy4N0jHztZEy9x7djZxeQ%3D%3D&numOfRows=100&pageNo=1&basYm=202409 app listening on port 3000!"
   );
 });
